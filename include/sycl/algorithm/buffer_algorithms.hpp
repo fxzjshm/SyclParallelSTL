fxzjshm/SyclParallelSTL

commit 8d59e1a05a43c25b33cbbc1989fa8a4a2f019e70

Add patch for intel/llvm(20220219) + hip + gfx906

include/sycl/algorithm/buffer_algorithms.hpp
@@ -339,6 +339,11 @@ sycl_algorithm_descriptor compute_mapscan_descriptor(cl::sycl::device device,
   size_t local_mem_size =
     device.get_info<cl::sycl::info::device::local_mem_size>();
   size_t size_per_work_group = min(size, local_mem_size / sizeofB);
+  // patch for intel/llvm(20220219) + hip + gfx906
+  // for some reason a hipErrorOutOfMemory is triggered if not divided here
+  #ifdef SYCL_IMPLEMENTATION_ONEAPI
+  size_per_work_group = max(size_per_work_group / 2, static_cast<size_t>(1));
+  #endif
   if (size_per_work_group <= 0)
     return sycl_algorithm_descriptor { size };
 

commit 855f6b25dfdb923b10577d8bd47563882088c628

buffer_algorithms: always limit shared memory usage
According to [`rocprim::detail::limit_block_size`](https://github.com/ROCm/rocPRIM/blob/e55fbf53ac07f5ffadfab6c52ae544af5914ba84/rocprim/include/rocprim/device/config_types.hpp#L106),
```
// Although GPUs have 64KiB, 32KiB is used here as a "soft" limit,
// because some additional memory may be required in kernels
```

Fixes: 8d59e1a ("Add patch for intel/llvm(20220219) + hip + gfx906")
Signed-off-by: fxzjshm <fxzjshm@163.com>

include/sycl/algorithm/buffer_algorithms.hpp
@@ -354,12 +354,10 @@ sycl_algorithm_descriptor compute_mapscan_descriptor(cl::sycl::device device,
     return sycl_algorithm_descriptor {};
   size_t local_mem_size =
     device.get_info<cl::sycl::info::device::local_mem_size>();
-  size_t size_per_work_group = min(size, local_mem_size / sizeofB);
-  // patch for intel/llvm(20220219) + hip + gfx906
-  // for some reason a hipErrorOutOfMemory is triggered if not divided here
-  #ifdef SYCL_IMPLEMENTATION_ONEAPI
-  size_per_work_group = max(size_per_work_group / 2, static_cast<size_t>(1));
-  #endif
+  // "/ 2" is used here as a "soft" limit,
+  // because some additional memory may be required in kernels
+  // ref: rocprim::detail::limit_block_size
+  size_t size_per_work_group = min(size, local_mem_size / 2 / sizeofB);
   if (size_per_work_group <= 0)
     return sycl_algorithm_descriptor { size };
 